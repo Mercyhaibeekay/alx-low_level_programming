51829d6ca370e3c89d75c833208264bf4bc05435
Mercyhaibeekay <mercyumufo@gmail.com>
2023-06-29 15:40:27 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * puts2 -print a string from the first char
+ * puts2 - to print a string from the first char
  * @str: string variable
  * Return: void
  */
 
 void puts2(char *str)
 {
-        int i = 0;
-
-        while (str[i] != '\0')
-        {
-                if (i % 2 == 0)
-                {
-                        _putchar(str[i]);
-                }
-                i++;
-        }
-        _putchar('\n');
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
 }
