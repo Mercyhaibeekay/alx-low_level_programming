17d79f195dfeade678e0062917424ea2516c9d6e
Mercyhaibeekay <mercyumufo@gmail.com>
2023-06-29 15:34:21 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-
 
 /**
- * cap_string - Capitalizes most of the words in a string.
- * @s: analized string.
- * Return: String with all words capitalized.
+ * puts2 -print a string from the first char
+ * @str: string variable
+ * Return: void
  */
 
-char *cap_string(char *s)
+void puts2(char *str)
 {
-int i, j;
-int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-i = 0;
-while (*(s + i) != '\0')
-{
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
-{
-if (i == 0)
-{
-*(s + i) = *(s + i) - 32;
-}
-else
-{
-for (j = 0; j <= 12; j++)
-{
-if (a[j] == *(s + i - 1))
-{
-*(s + i) = *(s + i) - 32;
-}
-}
-}
-}
-i++;
-}
-return (s);
+        int i = 0;
+
+        while (str[i] != '\0')
+        {
+                if (i % 2 == 0)
+                {
+                        _putchar(str[i]);
+                }
+                i++;
+        }
+        _putchar('\n');
 }
