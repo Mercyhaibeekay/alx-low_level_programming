2b88ee249f6f92f434473e71502a4fa5780b6a1f
Mercyhaibeekay <mercyumufo@gmail.com>
2023-07-01 05:45:38 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,65 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
- * cap_string - it capitalizes most of the words in a string.
- * @s: analized string.
+ * cap_string - it capitalizes all words of a string.
+ * @str: The string to be capitalized.
  *
- * Return: String with all words capitalized.
+ * Return: A pointer to the changed string.
  */
 
-char *cap_string(char *s)
-{
-int i, j;
-int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+char *cap_string(char *str)
 
-i = 0;
-while (*(s + i) != '\0')
 {
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
-{
-if (i == 0)
-{
-*(s + i) = *(s + i) - 32;
-}
-else
-{
-for (j = 0; j <= 12; j++)
-{
-if (a[j] == *(s + i - 1))
-{
-*(s + i) = *(s + i) - 32;
-}
-}
-}
-}
-i++;
-}
-return (s);
+
+        int index = 0;
+
+
+        while (str[index])
+
+        {
+
+                while (!(str[index] >= 'a' && str[index] <= 'z'))
+
+                        index++;
+
+
+                if (str[index - 1] == ' ' ||
+
+                    str[index - 1] == '\t' ||
+
+                    str[index - 1] == '\n' ||
+
+                    str[index - 1] == ',' ||
+
+                    str[index - 1] == ';' ||
+
+                    str[index - 1] == '.' ||
+
+                    str[index - 1] == '!' ||
+
+                    str[index - 1] == '?' ||
+
+                    str[index - 1] == '"' ||
+
+                    str[index - 1] == '(' ||
+
+                    str[index - 1] == ')' ||
+
+                    str[index - 1] == '{' ||
+
+                    str[index - 1] == '}' ||
+
+                    index == 0)
+
+                        str[index] -= 32;
+
+
+                index++;
+
+        }
+
+
+        return (str);
+
 }
