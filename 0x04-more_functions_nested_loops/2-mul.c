bf20a9e5e71070f675bf6d7aa6b73752de4cf893
Mercyhaibeekay <mercyumufo@gmail.com>
2023-07-06 11:30:03 -0700

    task 1 and 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,20 @@
 #include "main.h"
+
 /**
- * mul - multiply 2 integers.
- * @a: first integer
- * @b: second integer
- * Return: add the multiplication
+ * mul - it multiply two numbers
+ * Description: multiply two numbers
+ * @num1: interger value
+ * @num2: interger value
+ *
+ * Return: result
+ * Example:
+ *      mul(4 * 5) ---> 20
  */
-int mul(int a, int b)
+
+int mul(int num1, int num2)
 {
-return (a * b);
+	int result = 0;
+
+	result = num1 * num2;
+	return (result);
 }
