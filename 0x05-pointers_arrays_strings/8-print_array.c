abceb0c8d17946e0199d060334e08e2725ee8760
Mercyhaibeekay <mercyumufo@gmail.com>
2023-07-09 00:11:31 -0700

    task 8 and 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_array - it print `n` elements of an array of integers
+ * print_array - Print `n` elements of an array of integers
  * @a: int type array pointer
  * @n: int type integer
  * Description: Numbers must be separated by comma and space.
  * Numbers should be displayed in the same order they are stored in array.
- * You can only use _putchar to print.
  */
-
 void print_array(int *a, int n)
 {
-int index;
-index = 0; 
-for (n--; n >= 0; n--, index++)
+int i;
+i = 0;
+for (n--; n >= 0; n--, i++)
 {
-printf("%d", a[index]);
+printf("%d", a[i]);
 if (n > 0)
 {
 printf(", ");
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * *_strcpy -  it copies the string pointed to by src
- * @dest: char type string
- * @src: char type string
- * Description: Copy the string pointed to by pointer `src` to
- * the buffer pointed to by `dest`
- * Return: Pointer to `dest`
+ * *_strcpy - it copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
-int index = -1;
-do
+int len, i;
+len = 0;
+while (src[len] != '\0')
 {
-index++;
-dest[index] = src[index];
+len++;
 }
-while (src[index] != '\0');
+for (i = 0; i < len; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
 return (dest);
 }
